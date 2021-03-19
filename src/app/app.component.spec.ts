d942f5f9b5df8bfefa974b944838f17b199e748f
Kevin Boyle <kevin.michael.boyle@gmail.com>
2021-03-19 16:29:26 -0400

    bad test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'gcloud-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gcloud-angular');
+    expect(app.title).toEqual('gcloud-angular-is-great');
   });
 
   it('should render title in a h1 tag', () => {
